bcc1bb3173e9ce66da2ca2f1ceaa56dc2712672c
David <davidlarson46@yahoo.com>
2018-10-09 08:44:19 -0600

    Created model folder and added a file.

---
+++ FirstIOSApp2/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp2
+//
+//  Created by Larson, David on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
